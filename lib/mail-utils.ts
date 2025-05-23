1031636470e2739c015a6c9e7e6781e1d70f76ed
moosa <moosaabdullahi45@gmail.com>
2025-05-23 10:20:16 +0100

    add mail

--- lib/mail-utils.ts
+++ lib/mail-utils.ts
@@ -5,8 +5,8 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 const transport = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: parseInt(process.env.MAIL_PORT || '465'),
-    secure: process.env.NODE_ENV !== 'development',
-    // secure: true,
+    // secure: process.env.NODE_ENV !== 'development',
+    secure: true,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
